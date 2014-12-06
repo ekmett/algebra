5f7b80edafe61c0a435b09dc5b97b07db0cc2824
Daniel Fox Franke <dfoxfranke@gmail.com>
2014-12-06 16:28:24 -0500

    Move `gcd'` to a more appropriate place
    Also fix a bug: `gcd [x] = leadingUnit x` makes no sense.

--- src/Numeric/Domain/Euclidean.hs
+++ src/Numeric/Domain/Euclidean.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE CPP, ConstraintKinds, FlexibleContexts, FlexibleInstances     #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes #-}
 {-# LANGUAGE RebindableSyntax, UndecidableInstances                        #-}
-module Numeric.Domain.Euclidean (Euclidean(..), prs, gcd', chineseRemainder) where
+module Numeric.Domain.Euclidean (Euclidean(..), prs, chineseRemainder) where
 import Numeric.Additive.Group
 import Numeric.Algebra.Class
 import Numeric.Algebra.Unital
-import Numeric.Algebra.Unital.UnitNormalForm
 import Numeric.Decidable.Zero
 import Numeric.Domain.Internal
 import Numeric.Ring.Class
-import Prelude (fst, snd, otherwise, (.))
+import Prelude (otherwise)
 import qualified Prelude                 as P
 
 prs :: Euclidean r => r -> r -> [(r, r, r)]
@@ -23,12 +22,6 @@ prs f g = step [(g, 0, 1), (f, 1, 0)]
             t''       = (t - q * t')
         in step ((r - q * r', s'', t'') : acc)
     step _ = P.error "cannot happen!"
-
-gcd' :: Euclidean r => [r] -> r
-gcd' []     = one
-gcd' [x]    = leadingUnit x
-gcd' [x,y]  = gcd x y
-gcd' (x:xs) = gcd x (gcd' xs)
 
 chineseRemainder :: Euclidean r
                  => [(r, r)] -- ^ List of @(m_i, v_i)@
--- src/Numeric/Domain/GCD.hs
+++ src/Numeric/Domain/GCD.hs
@@ -1,3 +1,11 @@
-module Numeric.Domain.GCD (GCDDomain(..)) where
+module Numeric.Domain.GCD (GCDDomain(..), gcd') where
 
 import Numeric.Domain.Internal(GCDDomain(..))
+import Numeric.Algebra.Unital
+import Prelude()
+
+gcd' :: GCDDomain r => [r] -> r
+gcd' []     = one
+gcd' [x]    = x
+gcd' [x,y]  = gcd x y
+gcd' (x:xs) = gcd x (gcd' xs)
